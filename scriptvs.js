8fd2f34ad05bebfb5f5611e0ed30bb88f8e02a67
menmozhii <125111018+menmozhii@users.noreply.github.com>
2023-05-16 11:50:39 +0530

    Add files via upload

---
+++ scriptvs.js
@@ -0,0 +1,17 @@
+var p1=fetch("https://restcountries.com/v3.1/all") //it is a promise
+.then((data)=> data.json())//again here it is a promise
+
+.then((data1)=>foo(data1));
+
+function foo(data1){
+    console.log(data1);
+    for(var i=0;i<data1.length;i++){
+       
+        var div=document.createElement("div");
+        div.innerHTML=`Countryname:${data1[i].name.common} another:${data1[i].borders[0]} 
+         country code:${data1.altspellings[0]}
+         region:${data1[1].region} `;
+        document.body.append(div);
+    }
+
+}
